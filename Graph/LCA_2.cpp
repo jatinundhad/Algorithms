d33d19c221f790baaf90ef46dbc8076f32b49e4c
Jatin Undhad <jatinundhad33@gmail.com>
2023-06-14 00:45:00 +0530

    LCA-2 added.

--- Graph/LCA_2.cpp
+++ Graph/LCA_2.cpp
@@ -1,81 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define N 100001
-#define M 20
-vector<int> adjList[N];
-int Par[N][M], depth[N];
+const int N = 1e5, M = 20;
 
+vector<int> adj[N];
+int Par[N][M], tin[N], tout[N], timer = 0;
+
+// O(N*M)
 void dfs(int cur, int par)
 {
-    depth[cur] = depth[par] + 1;
+
+    tin[cur] = ++timer;
 
     Par[cur][0] = par;
-
-    for (int i = 1; i < M; i++)
+    for (int j = 1; j < M; j++)
     {
-        Par[cur][i] = Par[Par[cur][i - 1]][i - 1];
+        Par[cur][j] = Par[Par[cur][j - 1]][j - 1];
     }
 
-    for (auto &x : adjList[cur])
+    for (auto x : adj[cur])
     {
         if (x != par)
         {
             dfs(x, cur);
         }
     }
+
+    tout[cur] = ++timer;
 }
 
-// binary lifting
-int LCA(int a, int b)
+bool is_ancestor(int u, int v)
 {
-    if (a == b)
-        return a;
-
-    if (depth[a] < depth[b])
-        swap(a, b);
-
-    int diff = depth[a] - depth[b];
-    for (int j = M - 1; j >= 0; j--)
-    {
-        if ((diff >> j) & 1)
-        {
-            a = Par[a][j];
-        }
-    }
-
-    for (int j = M - 1; j >= 0; j--)
-    {
-        if (Par[a][j] != Par[b][j])
-        {
-            a = Par[a][j];
-            b = Par[b][j];
-        }
-    }
-
-    // Par[b][0];
-    return Par[a][0];
+    return tin[u] <= tin[v] && tout[u] >= tout[v];
 }
 
-int32_t main()
+// O(M) = logN
+int LCA(int u, int v)
 {
-    int n;
-    cin >> n;
+    if (is_ancestor(u, v))
+        return u;
+    if (is_ancestor(v, u))
+        return v;
 
-    int x, y;
-    for (int i = 1; i < n; i++)
+    for (int i = M - 1; i >= 0; --i)
     {
-        cin >> x >> y;
-        adjList[x].push_back(y);
-        adjList[y].push_back(x);
+        if (!is_ancestor(Par[u][i], v))
+            u = Par[u][i];
     }
 
-    dfs(1, 0);
+    return Par[u][0];
+}
 
-    cout << LCA(8, 9) << endl;
-    cout << LCA(4, 9) << endl;
-    cout << LCA(11, 9) << endl;
-    cout << LCA(8, 11) << endl;
-
-    return 0;
+void preprocess()
+{
+    dfs(1, 1);
 }
